3eeea0c94be9fd5067dcf9d7b702aac4c19e82c8
NotGhoul <58859813+NotGhoull@users.noreply.github.com>
2023-04-29 12:54:10 +0100

    Added basic functionallity
    Added basic functionallity the code now supports editing plain-text or editing HTML

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-alert("loaded");
+document.addEventListener("click", function(event) {
+  if (debugMode) {
+    alert("Clicked element: " + event.target.tagName + "\n\n" + event.target.outerHTML);
+  } else {
+    var choice = prompt("Enter 'text' to edit the text or 'html' to edit the HTML:", "text");
+    if (choice === "text") {
+      var newText = prompt("Enter new text:", event.target.innerText);
+      if (newText !== null) {
+        event.target.innerText = newText;
+      }
+    } else if (choice === "html") {
+      var newHTML = prompt("Enter new HTML:", event.target.outerHTML);
+      if (newHTML !== null) {
+        event.target.outerHTML = newHTML;
+      }
+    } else {
+      alert("Invalid choice: " + choice);
+    }
+  }
+});
+
+var debugMode = false;
+alert("Custom Inspect v1 injected successfully, press CTRL+R or refresh the page to disable");
